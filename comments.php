999fbfe2bf7bfa810146d0adb5b69ce2f2c0302c
joeroguen <joeroguen@gmail.com>
2018-07-10 09:48:01 -0600

    add comment pagination to comments.php

--- comments.php
+++ comments.php
@@ -1,5 +1,6 @@
 <div class="mtb-comments">
     <h2>comments</h2>
+    <?php paginate_comments_links( array( 'next_text' => 'Next' ) ); ?>
     <?php wp_list_comments( array( 'max_depth' => '1' ) ); ?>
     <?php comment_form(); ?>
 </div>
