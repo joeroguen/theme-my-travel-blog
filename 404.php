907a3b20658a84fd8b0fb96791d1bc55fe6e2c48
joeroguen <joeroguen@gmail.com>
2018-07-22 21:53:24 -0600

    add 404.php

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+
+<!-- sectioncontent -->
+<div class='content container'>
+    <p class='notice'>Sorry, but that was not found.</p>
+    <p>Please search something else.</p>
+</div>
+
+
+
+<?php get_footer(); ?>
